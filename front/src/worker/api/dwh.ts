a515c145d1165a7e5da1b7fe69795055b500392c
yanbin <yanb25@mail2.sysu.edu.cn>
2019-04-15 17:58:21 +0800

    forward server.bensyan.top:15022 to my ubuntu and change url.

--- front/src/worker/api/dwh.ts
+++ front/src/worker/api/dwh.ts
@@ -27,9 +27,9 @@ const DEFAULT_NODES: t.INodes = {
     // livenet: 'https://dwh.livenet.sonm.com:15022/DWHServer/',
     // rinkeby: 'https://dwh-testnet.sonm.com:15022/DWHServer/',
     // testrpc: 'https://proxy.test.sonm.com:15123/DWHServer/',
-    livenet: 'http://localhost:15022/DWHServer',
-    rinkeby: 'http://localhost:15022/DWHServer',
-    testrpc: 'http://localhost:15022/DWHServer',
+    livenet: 'http://server.bensyan.top:15022/DWHServer/',
+    rinkeby: 'http://server.bensyan.top:15022/DWHServer/',
+    testrpc: 'http://server.bensyan.top:15022/DWHServer/',
 };
 
 const MB_SIZE = 1000 * 1000;
