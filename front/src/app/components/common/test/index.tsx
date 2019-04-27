761e6a78a2ab501366fb7c4837491c28841ddef1
yanbin <yanb25@mail2.sysu.edu.cn>
2019-04-27 20:11:09 +0800

    change place order box. change Test class implementation.

--- front/src/app/components/common/test/index.tsx
+++ front/src/app/components/common/test/index.tsx
@@ -1,4 +1,10 @@
+// TODO: css not work. why?
 import * as React from 'react';
+import * as cn from 'classnames';
+import { FormField } from '../form';
+import { Input } from 'app/components/common/input';
+import { IChangeParams } from 'app/components/common/types';
+import { Button } from 'app/components/common/button';
 
 export interface ITestProps {
     nameTest?: string;
@@ -7,37 +13,59 @@ export interface ITestProps {
     updateStateProp?: any;
 }
 
+export interface OrderData {
+    price: string;
+    other: string;
+}
+
 export class Test extends React.Component<ITestProps, any> {
-    protected testClick = (event: any) => {
-        let o = {
-            nameTest: this.props.nameTest,
-            contentTest: this.props.contentTest,
+    constructor(props: any) {
+        super(props);
+        this.placeOrderData = {
+            price: '',
+            other: '',
         };
-        console.log(o);
-        console.log('ok');
+    }
+
+    protected placeOrderData: OrderData;
+
+    protected placeOrder = (event: any) => {
+        console.log(this.placeOrderData);
+    };
+    protected handleChangeInput = (params: IChangeParams<string>) => {
+        const key = params.name as keyof OrderData;
+        const value: OrderData[keyof OrderData] = params.value;
+        console.log(`field ${key} change to ${value}`);
+        this.placeOrderData[key] = value;
+        console.log(this.placeOrderData);
     };
 
     public render() {
-        return (
-            <div>
-                <span> name</span>
-                <input
-                    type="text"
-                    className={this.props.className}
-                    value={this.props.nameTest}
-                    name="nameTest"
-                    onChange={this.props.updateStateProp}
-                />
-                <span> content</span>
-                <input
-                    type="text"
-                    className={this.props.className}
-                    value={this.props.contentTest}
-                    name="contentTest"
-                    onChange={this.props.updateStateProp}
-                />
+        let p = this.props;
 
-                <button onClick={this.testClick}> Test</button>
+        return (
+            <div className={cn('order-filter-panel-place', p.className)}>
+                <div className="order-filter-panel-place__filters">
+                    <h3 className="order-filter-panel-place__header">
+                        Place Order
+                    </h3>
+                    <FormField className="order-filter-panel-place__address">
+                        <Input
+                            name="price"
+                            prefix="Price"
+                            onChange={this.handleChangeInput}
+                        />
+
+                        <Input
+                            name="other"
+                            prefix="Other"
+                            onChange={this.handleChangeInput}
+                        />
+                    </FormField>
+                    <Button square onClick={this.placeOrder}>
+                        Place Order
+                    </Button>
+                </div>
             </div>
         );
     }
